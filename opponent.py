0738c341b245a82b382e1119172e8604816c1361
AlexCozlovschi <62204345+AlexCozlovschi@users.noreply.github.com>
2020-08-05 11:10:56 +0300

    Update opponent.py

--- opponent.py
+++ opponent.py
@@ -1,12 +1,64 @@
-opponent_stats = [0, 0, 0]
-hp = opponent_stats[0]
-damage = opponent_stats[1]
-armor = opponent_stats[2]
+import Stats
+
+
+class Opponent:
+    def __init__(self, hp, damage, armor):
+        self.hp = hp
+        self.damage = damage
+        self.armor = armor
+
+    def attac(self, harm):
+        if self.hp > 0:
+            self.hp = self.hp - harm
+        else:
+            print('death')
+
+
+enemy = Opponent(0, 0, 0)
+
+
+def target_detection():
+    if Stats.x in range(Stats.posi[0], Stats.posi[0] + Stats.posi[1]):
+        if Stats.y in range(Stats.posi[2], Stats.posi[2] + Stats.posi[3]):
+            if Stats.Bullet > 0:
+                enemy.attac(10)
 
 
 def slime():
-    return [100, 5, 0]
+    global enemy
+    hp = 100
+    damage = 5
+    armor = 0
+    enemy = Opponent(hp, damage, armor)
 
 
 def knight():
-    return [150, 10, 10]
+    global enemy
+    hp = 100
+    damage = 10
+    armor = 20
+    enemy = Opponent(hp, damage, armor)
+
+
+def boss():
+    global enemy
+    hp = 200
+    damage = 10
+    armor = 0
+    enemy = Opponent(hp, damage, armor)
+
+
+if Stats.enemy_type == 0:
+    enemy = Opponent(0, 0, 0)
+elif Stats.enemy_type == 1:
+    slime()
+elif Stats.enemy_type == 2:
+    knight()
+elif Stats.enemy_type == 3:
+    boss()
+
+
+
+
+
+
